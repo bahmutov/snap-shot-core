b49cd0ae01a0ce945ba1d2dc0e902fe735cfed8a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-16 15:54:58 -0500

    initial code

---
+++ src/snap-shot-core-spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* global describe, it */
+describe('snap-shot-core', () => {
+  const snapShotCore = require('.')
+  it('write this test', () => {
+    console.assert(snapShotCore, 'should export something')
+  })
+})
